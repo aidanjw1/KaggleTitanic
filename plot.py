40128f6dbe315a286b640430d7f151b125cfd5fe
Aidan White <aidan.white36@gmail.com>
2017-10-04 10:52:49 +0200

    Made a double bar chart in plot of ages

--- plot.py
+++ plot.py
@@ -4,34 +4,72 @@ from collections import Counter
 
 people = titanic.people
 
-print people
+def age_survivors():
+    ages = []
+    Survived = []
 
-ages = []
-Survived = []
+    dead = []
+    age_dead = []
 
-for person in people:
-    if person['Survived'] == '1':
-        Survived.append(person['Survived'])
-        if person['Age'] == "":
-            continue
-        else:
-            ages.append(float(person['Age']))
+    fig, ax = plt.subplots(2)
 
-#ages = ages[1:]
-#Survived = Survived[1:]
-print ages
-print Survived
+    for person in people:
+        if person['Survived'] == '1':
+            Survived.append(person['Survived'])
+            if person['Age'] == "":
+                continue
+            else:
+                ages.append(float(person['Age']))
 
-c = Counter(ages)
+        elif person['Survived'] == '0':
+            if person['Age'] == "":
+                continue
+            else:
+                age_dead.append(float(person['Age']))
 
-print c
+
+    #ages = ages[1:]
+    #Survived = Survived[1:]
+    print ages
+    print Survived
+
+    c = Counter(ages)
+    c_dead = Counter(age_dead)
+
+    print c
 
 
-plt.bar(c.keys(), c.values())
-plt.xlabel("Age")
-plt.ylabel("Number of Survivors")
-plt.title("Ages of Titanic Survivors")
-plt.show()
+    ax[0].bar(c.keys(), c.values(), color='g')
+    plt.xlabel("Age")
+    plt.ylabel("Number of Survivors")
+    plt.title("Ages of Titanic Survivors")
+    fig.show()
+
+    plt.bar(c_dead.keys(), c_dead.values())
+    plt.xlabel("Age")
+    plt.ylabel("Number of Deads")
+    plt.title("Ages of Titanic Deads")
+    fig.show()
+    plt.show()
+
+def sex_survivors():
+    for person in people:
+        males = 0
+        females = 0
+        if person['Survived'] == '1':
+            if person['Sex'] == "male":
+                males += 1
+            elif person['Sex'] == 'female':
+                females += 1
+    print males
+    print females
+
+def main():
+    age_survivors()
+    #sex_survivors()
+
+main()
+
 
 
 
