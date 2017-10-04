92d4e8a2508123b8163339add6df6c295ef2f115
Aidan White <aidan.white36@gmail.com>
2017-10-04 11:21:22 +0200

    Inted the class

--- titanic.py
+++ titanic.py
@@ -50,6 +50,7 @@ def get_target_list():
 peeps = []
 for person in sorted(people):
     try:
-        peeps.append([person['Sex'], person['Pclass'], float(person['Age'])//10])
+        peeps.append([int(float(person['Age'])//10), person['Sex'], int(person['Pclass'])])
     except:
         continue
+print sorted(peeps)
